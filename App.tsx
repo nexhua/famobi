2db70fdcb9627034f58b548345f7782e60bbd1c2
nexhua <CChristofear@hotmail.com>
2023-06-03 19:34:21 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomePage from './src/HomePage';
+
+function App(): JSX.Element {
+  return <HomePage />;
+}
+
+export default App;
---
+++ src/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function HomePage(): JSX.Element {
+  return (
+    <View>
+      <Text>hellasdwqeo</Text>
+    </View>
+  );
+}
+
+export default HomePage;
